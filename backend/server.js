49208efee8c0fe2846c609c57c42f4551780633e
Manmith7 <manmithgopari7@gmail.com>
2025-06-28 19:57:10 +0530

    Login/SignUp

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(cors())
 app.use(express.json());
 
-app.use('/',AuthRoutes);
+app.use('/api/auth/',AuthRoutes);
 const port = process.env.PORT;
 app.listen(port,()=>{
     console.log(`Server running on ${port}`);
--- frontend/src/components/UserAuth/Login.jsx
+++ frontend/src/components/UserAuth/Login.jsx
@@ -11,7 +11,7 @@ function Login({ setIsLoggedIn, setUserName, setActiveCategory }) {
     setErrorMessage(""); // Clear previous errors
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://ecommerce-o1mq.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ emailOrMobile, password }),
