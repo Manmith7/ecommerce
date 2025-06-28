58fe56db35f0835c4ece5bf82d7759e664f2ee53
Manmith7 <manmithgopari7@gmail.com>
2025-06-28 20:14:15 +0530

    Login/SignUp

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(cors())
 app.use(express.json());
 
-app.use('/api/auth/',AuthRoutes);
+app.use('/api/auth',AuthRoutes);
 
 const port = process.env.PORT;
 app.listen(port,()=>{
